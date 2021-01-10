e44e65356a67e91faabb308a22cf691f5ef4dbb6
MaartenGr <maarten_grootendorst@hotmail.com>
2021-01-10 08:07:48 +0100

    Fix import error

--- bertopic/__init__.py
+++ bertopic/__init__.py
@@ -1,12 +1,11 @@
 from bertopic._bertopic import BERTopic
 from bertopic._ctfidf import ClassTFIDF
-from bertopic._embeddings import languages, embedding_models
+from bertopic._embeddings import languages
 
 __version__ = "0.4.2"
 
 __all__ = [
     "BERTopic",
     "ClassTFIDF",
-    "languages",
-    "embedding_models",
+    "languages"
 ]
