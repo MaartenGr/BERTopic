e3f303d34a1d38446accde2cb0ae1d1859bd6fa6
Maarten Grootendorst <maarten_grootendorst@hotmail.com>
2021-02-05 10:32:14 +0100

    Fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,5 @@ import pytest
 def base_bertopic():
     model = BERTopic(language="english",
                      verbose=True,
-                     n_neighbors=5,
                      min_topic_size=5)
     return model
--- tests/test_other.py
+++ tests/test_other.py
@@ -22,7 +22,6 @@ def test_load_save_model():
     assert model.language == loaded_model.language
     assert model.embedding_model == loaded_model.embedding_model
     assert model.top_n_words == loaded_model.top_n_words
-    assert model.n_neighbors == loaded_model.n_neighbors
 
 
 def test_get_params():
@@ -32,7 +31,6 @@ def test_get_params():
     assert not params["embedding_model"]
     assert not params["low_memory"]
     assert not params["nr_topics"]
-    assert not params["stop_words"]
     assert params["n_neighbors"] == 15
     assert params["n_gram_range"] == (1, 1)
     assert params["min_topic_size"] == 10
