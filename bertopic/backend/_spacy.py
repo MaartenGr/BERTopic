eac46eaed79dba7f29401e14258512d10119a706
Maarten Grootendorst <maarten_grootendorst@hotmail.com>
2021-04-02 13:08:36 +0100

    Test gpu requirement for spacy transformers

--- bertopic/backend/_spacy.py
+++ bertopic/backend/_spacy.py
@@ -11,15 +11,16 @@ class SpacyBackend(BaseEmbedder):
 
         if "spacy" in str(type(embedding_model)):
             self.embedding_model = embedding_model
+
+            if "transformer" in self.embedding_model.component_names:
+                set_gpu_allocator("pytorch")
+                require_gpu(0)
         else:
             raise ValueError("Please select a correct Spacy model by either using a string such as 'en_core_web_md' "
                              "or create a nlp model using: `nlp = spacy.load('en_core_web_md')")
 
     def embed(self, documents, verbose):
         if "transformer" in self.embedding_model.component_names:
-            set_gpu_allocator("pytorch")
-            require_gpu(0)
-
             embeddings = np.array([self.embedding_model(doc)._.trf_data.tensors[-1][0] for
                                    doc in tqdm(documents, disable=not verbose)])
         else:
