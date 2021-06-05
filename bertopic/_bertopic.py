e56bac2cd333d297f0d6fd0b5ca182f338d185dd
Nils Reimers <info@nils-reimers.de>
2021-06-05 14:36:43 +0200

    Update _bertopic.py

--- bertopic/_bertopic.py
+++ bertopic/_bertopic.py
@@ -55,7 +55,7 @@ class BERTopic:
     from sentence_transformers import SentenceTransformer
 
     docs = fetch_20newsgroups(subset='all')['data']
-    sentence_model = SentenceTransformer("paraphrase-mpnet-base-v2")
+    sentence_model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
     topic_model = BERTopic(embedding_model=sentence_model)
     ```
 
@@ -196,7 +196,7 @@ class BERTopic:
 
         # Create embeddings
         docs = fetch_20newsgroups(subset='all')['data']
-        sentence_model = SentenceTransformer("paraphrase-mpnet-base-v2")
+        sentence_model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
         embeddings = sentence_model.encode(docs, show_progress_bar=True)
 
         # Create topic model
@@ -247,7 +247,7 @@ class BERTopic:
 
         # Create embeddings
         docs = fetch_20newsgroups(subset='all')['data']
-        sentence_model = SentenceTransformer("paraphrase-mpnet-base-v2")
+        sentence_model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
         embeddings = sentence_model.encode(docs, show_progress_bar=True)
 
         # Create topic model
@@ -333,7 +333,7 @@ class BERTopic:
 
         # Create embeddings
         docs = fetch_20newsgroups(subset='all')['data']
-        sentence_model = SentenceTransformer("paraphrase-mpnet-base-v2")
+        sentence_model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
         embeddings = sentence_model.encode(docs, show_progress_bar=True)
 
         # Create topic model
