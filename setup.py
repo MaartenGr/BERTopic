393500c6d2a82a34f2fe17d8cecbe8bffbe62701
MaartenGr <maarten_grootendorst@hotmail.com>
2021-06-08 07:38:25 +0200

    Update changelog

--- setup.py
+++ setup.py
@@ -23,10 +23,8 @@ base_packages = [
 ]
 
 flair_packages = [
-    "numpy<1.20.0",
-    "torch>=1.5.0,<1.8.0",
-    "transformers>=4.0.0",
-    "flair==0.8"
+    "torch>=1.4.0,<1.7.1",
+    "flair==0.7"
 ]
 
 spacy_packages = [
