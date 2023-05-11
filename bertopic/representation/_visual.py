9d3b7a2b089dc8fd71fa90131a0d5509163ed4de
MaartenGr <maartengrootendorst@gmail.com>
2023-05-11 15:26:01 +0200

    Additional options for resizing images

--- bertopic/representation/_visual.py
+++ bertopic/representation/_visual.py
@@ -19,7 +19,13 @@ class VisualRepresentation(BaseRepresentation):
     Arguments:
         nr_repr_images: Number of representative images to extract
         nr_samples: The number of candidate documents to extract per cluster.
-        image_size: The size of the collage for each topic (width x height)
+        image_height: The height of the resulting collage
+        image_square: Whether to resize each image in the collage
+                      to a square. This can be visually more appealing
+                      if all input images are all almost squares.
+        image_to_text_model: The model to caption images.
+        batch_size: The number of images to pass to the 
+                    `image_to_text_model`. 
 
     Usage:
 
@@ -41,20 +47,22 @@ class VisualRepresentation(BaseRepresentation):
     def __init__(self,
                  nr_repr_images: int = 9,
                  nr_samples: int = 500,
-                 image_size: Tuple[int, int] = (600, 600),
-                 text_to_image_model: Union[str, Pipeline] = None,
+                 image_height: Tuple[int, int] = 600,
+                 image_squares: bool = False,
+                 image_to_text_model: Union[str, Pipeline] = None,
                  batch_size: int = 32):
         self.nr_repr_images = nr_repr_images
         self.nr_samples = nr_samples
-        self.image_size = image_size
+        self.image_height = image_height
+        self.image_squares = image_squares
 
         # Text-to-image model
-        if isinstance(text_to_image_model, Pipeline):
-            self.text_to_image_model = text_to_image_model
-        elif isinstance(text_to_image_model, str):
-            self.text_to_image_model = pipeline("image-to-text", model=text_to_image_model)
-        elif text_to_image_model is None:
-            self.text_to_image_model = None
+        if isinstance(image_to_text_model, Pipeline):
+            self.image_to_text_model = image_to_text_model
+        elif isinstance(image_to_text_model, str):
+            self.image_to_text_model = pipeline("image-to-text", model=image_to_text_model)
+        elif image_to_text_model is None:
+            self.image_to_text_model = None
         else:
             raise ValueError("Please select a correct transformers pipeline. For example:"
                              "pipeline('image-to-text', model='nlpconnect/vit-gpt2-image-captioning')")
@@ -79,7 +87,8 @@ class VisualRepresentation(BaseRepresentation):
         """
         # Extract image ids of most representative documents
         images = documents["Image"].values.tolist()
-        _, _, _, repr_docs_ids = topic_model._extract_representative_docs(c_tf_idf, 
+        (_, _, _, 
+         repr_docs_ids) = topic_model._extract_representative_docs(c_tf_idf, 
                                                                    documents, 
                                                                    topics,
                                                                    nr_samples=self.nr_samples,
@@ -92,14 +101,18 @@ class VisualRepresentation(BaseRepresentation):
             
             # Get and order represetnative images
             sliced_examplars = repr_docs_ids[topic+topic_model._outliers]
-            sliced_examplars = [sliced_examplars[i:i + 3] for i in range(0, len(sliced_examplars), 3)]
+            sliced_examplars = [sliced_examplars[i:i + 3] for i in 
+                                range(0, len(sliced_examplars), 3)]
             images_to_combine = [
-                    [Image.open(images[index]) if isinstance(images[index], str) else images[index] for index in sub_indices]
+                    [Image.open(images[index]) if isinstance(images[index], str) 
+                     else images[index] for index in sub_indices]
                 for sub_indices in sliced_examplars
             ]
 
             # Concatenate representative images
-            representative_image = get_concat_tile_resize(images_to_combine, self.image_size)
+            representative_image = get_concat_tile_resize(images_to_combine, 
+                                                          self.image_height,
+                                                          self.image_squares)
             representative_images[topic] = representative_image
 
             # Make sure to properly close images
@@ -111,8 +124,8 @@ class VisualRepresentation(BaseRepresentation):
         return representative_images
     
     def _convert_image_to_text(self,
-                      images: List[str],
-                      verbose: bool = False) -> List[str]:
+                               images: List[str],
+                               verbose: bool = False) -> List[str]:
         """ Convert a list of images to captions. 
 
         Arguments:
@@ -126,13 +139,13 @@ class VisualRepresentation(BaseRepresentation):
         if self.batch_size is not None:
             documents = []
             for batch in tqdm(self._chunks(images), disable=not verbose):
-                outputs = self.text_to_image_model(batch)
+                outputs = self.image_to_text_model(batch)
                 captions = [output[0]["generated_text"] for output in outputs]
                 documents.extend(captions)
 
         # Convert images to text
         else:
-            outputs = self.text_to_image_model(images)
+            outputs = self.image_to_text_model(images)
             documents = [output[0]["generated_text"] for output in outputs]
 
         return documents
@@ -154,7 +167,6 @@ class VisualRepresentation(BaseRepresentation):
             image_centroids[topic] = indices
             
         # Extract documents
-        from tqdm import tqdm
         documents = pd.DataFrame(columns=["Document", "ID", "Topic", "Image"])
         current_id = 0
         for topic, image_ids in tqdm(image_centroids.items()):
@@ -175,20 +187,21 @@ class VisualRepresentation(BaseRepresentation):
     def _chunks(self, images):     
         for i in range(0, len(images), self.batch_size):
             yield images[i:i + self.batch_size]
-    
+
 
 def get_concat_h_multi_resize(im_list):
     """
     Code adapted from: https://note.nkmk.me/en/python-pillow-concat-images/
     """
     min_height = min(im.height for im in im_list)
+    min_height = max(im.height for im in im_list)
     im_list_resize = []
     for im in im_list:
         im.resize((int(im.width * min_height / im.height), min_height), resample=0)
         im_list_resize.append(im)
 
     total_width = sum(im.width for im in im_list_resize)
-    dst = Image.new('RGB', (total_width, min_height))
+    dst = Image.new('RGB', (total_width, min_height), (255, 255, 255))
     pos_x = 0
     for im in im_list_resize:
         dst.paste(im, (pos_x, 0))
@@ -201,10 +214,11 @@ def get_concat_v_multi_resize(im_list):
     Code adapted from: https://note.nkmk.me/en/python-pillow-concat-images/
     """
     min_width = min(im.width for im in im_list)
+    min_width = max(im.width for im in im_list)
     im_list_resize = [im.resize((min_width, int(im.height * min_width / im.width)), resample=0)
                       for im in im_list]
     total_height = sum(im.height for im in im_list_resize)
-    dst = Image.new('RGB', (min_width, total_height))
+    dst = Image.new('RGB', (min_width, total_height), (255, 255, 255))
     pos_y = 0
     for im in im_list_resize:
         dst.paste(im, (0, pos_y))
@@ -212,9 +226,36 @@ def get_concat_v_multi_resize(im_list):
     return dst
 
 
-def get_concat_tile_resize(im_list_2d, image_size=(600, 600)):
+def get_concat_tile_resize(im_list_2d, image_height=600, image_squares=False):
     """
     Code adapted from: https://note.nkmk.me/en/python-pillow-concat-images/
     """
-    im_list_v = [get_concat_h_multi_resize(im_list_h) for im_list_h in im_list_2d]
-    return get_concat_v_multi_resize(im_list_v).resize(image_size)
+    images = [[image.copy() for image in images] for images in im_list_2d]
+    
+    # Create 
+    if image_squares:
+        width =  int(image_height / 3)
+        height = int(image_height / 3)
+        images = [[image.resize((width, height)) for image in images] for images in im_list_2d]
+    
+    # Resize images based on minimum size
+    else:
+        min_width = min([min([img.width for img in imgs]) for imgs in im_list_2d])
+        min_height = min([min([img.height for img in imgs]) for imgs in im_list_2d])
+        for i, imgs in enumerate(images):
+            for j, img in enumerate(imgs):
+                if img.height > img.width:
+                    images[i][j] = img.resize((int(img.width * min_height / img.height), min_height), resample=0)
+                elif img.width > img.height:
+                    images[i][j] = img.resize((min_width, int(img.height * min_width / img.width)), resample=0)
+                else:
+                    images[i][j] = img.resize((min_width, min_width))
+
+    # Resize grid image
+    images = [get_concat_h_multi_resize(im_list_h) for im_list_h in images]
+    img = get_concat_v_multi_resize(images)
+    height_percentage = (image_height/float(img.size[1]))
+    adjusted_width = int((float(img.size[0])*float(height_percentage)))
+    img = img.resize((adjusted_width, image_height), Image.Resampling.LANCZOS)
+    
+    return img
