63b569f5cf8e2c4eebb69182b727416365f0f270
Olivier Bougeant <36014848+Bougeant@users.noreply.github.com>
2024-03-19 23:43:54 +0200

    Fix CUML HDBSCAN predictions by using correct method.

--- bertopic/cluster/_utils.py
+++ bertopic/cluster/_utils.py
@@ -51,8 +51,8 @@ def hdbscan_delegator(model, func: str, embeddings: np.ndarray = None):
 
         str_type_model = str(type(model)).lower()
         if "cuml" in str_type_model and "hdbscan" in str_type_model:
-            from cuml.cluster.hdbscan.prediction import approximate_predict
-            probabilities = approximate_predict(model, embeddings)
+            from cuml.cluster.hdbscan.prediction import membership_vector
+            probabilities = membership_vector(model, embeddings)
             return probabilities
 
         return None
