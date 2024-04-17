Adds functionality that makes sure that the distances between clusters are unique (by adding some small noise) otherwise the flatting of the hierarchy would produce incorrect values for "Topics" for these clusters (#1907)

bertopic/_bertopic.py
@@ -54,7 +54,7 @@ from bertopic.dimensionality import BaseDimensionalityReduction
 from bertopic.cluster._utils import hdbscan_delegator, is_supported_hdbscan
 from bertopic._utils import (
     MyLogger, check_documents_type, check_embeddings_shape,
-    check_is_fitted, validate_distance_matrix
+    check_is_fitted, validate_distance_matrix, get_unique_distances
 )
 import bertopic._save_utils as save_utils
 
@@ -978,6 +978,11 @@ class BERTopic:
 
         # Use the 1-D condensed distance matrix as an input instead of the raw distance matrix
         Z = linkage_function(X)
+
+        # Ensuring that the distances between clusters are unique otherwise the flatting of the hierarchy with
+        # `sch.fcluster(...)` would produce incorrect values for "Topics" for these clusters
+        if len(Z[:, 2]) != len(np.unique(Z[:, 2])):
+            Z[:, 2] = get_unique_distances(Z[:, 2])
 
         # Calculate basic bag-of-words to be iteratively merged later
         documents = pd.DataFrame({"Document": docs,

tests/test_utils.py
@@ -1,7 +1,7 @@
 import pytest
 import logging
 import numpy as np
-from bertopic._utils import check_documents_type, check_embeddings_shape, MyLogger
+from bertopic._utils import check_documents_type, check_embeddings_shape, MyLogger, get_unique_distances
 
 
 def test_logger():
@@ -32,3 +32,11 @@ def test_check_embeddings_shape():
     embeddings = np.array([[1, 2, 3],
                            [2, 3, 4]])
     check_embeddings_shape(embeddings, docs)
+
+
+def test_make_unique_distances():
+    dists = np.array([0, 0, 0.5, 0.75, 1, 1])
+    unique_dists = get_unique_distances(dists)
+
+    assert len(unique_dists) == len(dists), "The number of elements must be the same"
+    assert len(dists) == len(np.unique(unique_dists)), "The distances must be unique"