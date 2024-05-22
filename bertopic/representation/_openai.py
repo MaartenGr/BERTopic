8a97198cf4db443bfa110e2c9f6192e272f1707e
Steven Solomon <steven-solomon@github.com>
2024-05-22 15:49:45 +0000

    Use requested logging prefix

--- bertopic/representation/_openai.py
+++ bertopic/representation/_openai.py
@@ -223,16 +223,16 @@ class OpenAI(BaseRepresentation):
                 if output.finish_reason == "stop":
                     label = output.message.content.strip().replace("topic: ", "")
                 elif output.finish_reason == "length":
-                    logger.warn(f"Extracing Topics - Length limit reached for doc_ids ({repr_doc_ids})")
+                    logger.warn(f"OpenAI Topic Representation - Length limit reached for doc_ids ({repr_doc_ids})")
                     if hasattr(output.message, "content"):
                         label = output.message.content.strip().replace("topic: ", "")
                     else:
-                        label = "Incomplete output due to token limit being reached"
+                        label = "OpenAI Topic Representation - Incomplete output due to token limit being reached"
                 elif output.finish_reason == "content_filter":
-                    logger.warn(f"Extracing Topics - Content filtered for doc_ids ({repr_doc_ids})")
+                    logger.warn(f"OpenAI Topic Representation - Content filtered for doc_ids ({repr_doc_ids})")
                     label = "Output content filtered by OpenAI"
                 else:
-                    logger.warn(f"Extracing Topics - No label due to finish_reason {output.finish_reason} for doc_ids ({repr_doc_ids})")
+                    logger.warn(f"OpenAI Topic Representation - No label due to finish_reason {output.finish_reason} for doc_ids ({repr_doc_ids})")
                     label = "No label returned"
             else:
                 if self.exponential_backoff:
