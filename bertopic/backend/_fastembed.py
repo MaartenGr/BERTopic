0c69272b572e8bea2f94613cb9402e811f1fa343
Nicola Procopio <nico.pro412@gmail.com>
2025-03-20 15:57:05 +0100

    Update _fastembed.py
    add parallel

--- bertopic/backend/_fastembed.py
+++ bertopic/backend/_fastembed.py
@@ -51,7 +51,7 @@ class FastEmbedBackend(BaseEmbedder):
             Document/words embeddings with shape (n, m) with `n` documents/words
             that each have an embeddings size of `m`
         """
-        embeddings_generator = self.embedding_model.embed(documents, show_progress_bar=verbose)
+        embeddings_generator = self.embedding_model.embed(documents, parallel=0, show_progress_bar=verbose)
         embeddings_list = list(embeddings_generator)
         embeddings_array = np.stack(embeddings_list)
         return embeddings_array
