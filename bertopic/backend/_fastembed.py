fd9739161d78ce9d32cc56b74c943e7af760bc4c
Nicola Procopio <nico.pro412@gmail.com>
2025-03-20 15:51:49 +0100

    Update _fastembed.py
    try to accelerate

--- bertopic/backend/_fastembed.py
+++ bertopic/backend/_fastembed.py
@@ -51,5 +51,7 @@ class FastEmbedBackend(BaseEmbedder):
             Document/words embeddings with shape (n, m) with `n` documents/words
             that each have an embeddings size of `m`
         """
-        embeddings = np.array(list(self.embedding_model.embed(documents, show_progress_bar=verbose)))
-        return embeddings
+        embeddings_generator = self.embedding_model.embed(documents, show_progress_bar=verbose)
+        embeddings_list = list(embeddings_generator)
+        embeddings_array = np.stack(embeddings_list)
+        return embeddings_array
